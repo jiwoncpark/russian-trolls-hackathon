5840ed10b3c0d098e1f1365fd847c9ab6d11a9b4
Enguerrand Horel <ehorel@stanford.edu>
2018-11-17 12:55:01 -0800

    Faster version of combined/main.py

--- ProjectTroll-master/data/combined/main.py
+++ ProjectTroll-master/data/combined/main.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+import time
 
 from datetime import datetime, timedelta
 import sys, os
@@ -27,26 +28,31 @@ pollster['start_date'] = pd.to_datetime(pollster['start_date'])
 #             pollster.loc[j,'start_date'],
 #             '%Y-%m-%d')
 
+pollster.sort_values(by=['start_date'], inplace=True)
+
 # assign tweets uniquely to polls
 print('assigning tweets to polls')
+start_time = time.time()
 for i in range(len(twitter)):
     # find all polls that this tweet affects
-    poll_idxs = []
+    twitter_publish = twitter.loc[i,'publish_date']
 
-    for j in range(len(pollster)):
-        twitter_publish = twitter.loc[i,'publish_date']
-        pollster_start = pollster.loc[j,'start_date']
-        
-        if (twitter_publish < pollster_start) \
-         & (twitter_publish > pollster_start - timedelta(days=days)):
-             poll_idxs.append(j)
+    poll_idxs = pollster.loc[(twitter_publish < pollster['start_date']) &\
+    (pollster['start_date'] < twitter_publish + timedelta(days=days))].index.values
+
+    # poll_idxs = []
+    # for j in range(len(pollster)):
+    #     pollster_start = pollster.loc[j,'start_date']
+    #     if (twitter_publish < pollster_start) \
+    #      & (twitter_publish > pollster_start - timedelta(days=days)):
+    #          poll_idxs.append(j)
              
     # assign tweet randomly to one of the polls
     if len(poll_idxs) > 0:
         twitter.loc[i,'poll_idx'] = np.random.choice(poll_idxs)
     else:
         twitter.loc[i,'poll_idx'] = None
-        
+print(time.time()-start_time)  
 # aggregate the tweets of each poll
 print('aggregating tweets belonging to the same poll')
 polls_with_tweets = []
