3346a71adab00a1d593698c7223ef2a4e9b8e2f3
jiwoncpark <jiwon.christine.park@gmail.com>
2018-11-23 11:11:35 -0800

    Revive loader unit tests

--- ProjectTroll-master/loader/basic_meta_data.py
+++ ProjectTroll-master/loader/basic_meta_data.py
@@ -1,4 +1,5 @@
 import torch
+import numpy as np
 import os
 from torchtext.vocab import GloVe
 from torchtext.data import Field, TabularDataset, Iterator, Pipeline
@@ -65,7 +66,10 @@ def basic_meta_data(obj):
               ('baseline_pred_right', VARIABLE),
               ('left', LABEL),
              ('mid', LABEL),
-             ('right', LABEL),]
+             ('right', LABEL),
+             ('7', None),
+             ('8', None),
+             ('9', None)]
     
     #train_csv = 'twitter_pollster_'+str(obj.days)+'_days_train_small.csv'
     #test_csv = 'twitter_pollster_'+str(obj.days)+'_days_test_small.csv'
@@ -102,5 +106,91 @@ def basic_meta_data(obj):
     
     return TEXT, vocab_size, word_embeddings, train_iter_, test_iter_
 
+if __name__== "__main__":
 
+    tokenize = lambda x: x.split()
+    TEXT = Field(sequential=True,
+                 tokenize=tokenize,
+                 lower=True,
+                 batch_first=True,
+                 fix_length=None)
 
+    VARIABLE = Field(sequential=False,
+                  dtype=torch.float,
+                  batch_first=True,
+                  use_vocab=False,)
+    
+    LABEL = Field(sequential=False,
+                  dtype=torch.float,
+                  batch_first=True,
+                  use_vocab=False)
+    
+    fields = [#('id', None),
+              ('content', TEXT),
+              ('avg_followers',VARIABLE),
+              ('avg_following', VARIABLE),
+              ('avg_left', VARIABLE),
+              ('avg_news', VARIABLE),
+              ('avg_right', VARIABLE),
+              ('time', VARIABLE),
+              ('baseline_pred_left', VARIABLE),
+              ('baseline_pred_mid', VARIABLE),
+              ('baseline_pred_right', VARIABLE),
+              ('left', LABEL),
+             ('mid', LABEL),
+             ('right', LABEL),
+             ('7', None),
+             ('8', None),
+             ('9', None)]
+    
+    train_csv = 'train1.csv'
+    test_csv = 'test1.csv'
+
+    train_dataset = TabularDataset(path='mydata/'+train_csv,
+                                   format='csv',
+                                   skip_header=True,
+                                   fields=fields)
+
+    test_dataset = TabularDataset(path='mydata/'+test_csv,
+                                  format='csv',
+                                  skip_header=True,
+                                  fields=fields)
+
+    TEXT.build_vocab(train_dataset, vectors=GloVe(name='twitter.27B',
+                                                  dim=25))
+    vocab_size = len(TEXT.vocab)
+    word_embeddings = TEXT.vocab.vectors
+    print ("vector size of text vocabulary: ", TEXT.vocab.vectors.size())
+
+    train_iter, test_iter = Iterator.splits(
+            (train_dataset, test_dataset),
+            sort_key=lambda x: len(x.content), 
+            batch_sizes=(7, 7),
+            device=torch.device('cpu'),
+            sort_within_batch=True,
+            repeat=False)
+    
+    print(train_csv, test_csv)
+    train_iter_ = BatchWrapper(train_iter, ['content', 'avg_followers', 'avg_following', 'avg_left', 'avg_news', 'avg_right', 'time', 'baseline_pred_left', 'baseline_pred_mid', 'baseline_pred_right'], ['left', 'mid', 'right'])
+    test_iter_ = BatchWrapper(test_iter, ['content', 'avg_followers', 'avg_following', 'avg_left', 'avg_news', 'avg_right', 'time', 'baseline_pred_left', 'baseline_pred_mid', 'baseline_pred_right'], ['left', 'mid', 'right'])
+
+    batch0 = None
+    batch1 = None
+    batch2 = None
+    for iter, batch in enumerate(train_iter_, 1):
+        if iter==1:
+            #print(iter, batch)
+            batch0 = batch[0]
+            batch1 = batch[1]
+            batch2 = batch[2]
+            print("batch[0]: ", batch[0])
+            print("batch[1]: ", batch[1]) # 6 metadata
+            print("batch[2]: ", batch[2])
+            print("batch[0] size: ", batch[0].shape)
+            print("batch[1] size: ", batch[1].shape)
+            print("batch[2] size: ", batch[2].shape)
+        break
+    
+    np.save('batch0', batch0)
+    np.save('batch1', batch1)
+    np.save('batch2', batch2)
